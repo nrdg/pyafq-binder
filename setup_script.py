b6b3945aa667310f7fc973194a082adc6e2d96d6
Ariel Rokem <arokem@gmail.com>
2021-04-27 15:35:03 -0700

    Run only the data download. Not the entire thing.

--- setup_script.py
+++ setup_script.py
@@ -1,19 +1,19 @@
-import os.path as op
+#import os.path as op
 import AFQ.data as afd
-from AFQ.definitions.mask import RoiMask
-from AFQ.definitions.mapping import AffMap
-from AFQ import api
+#from AFQ.definitions.mask import RoiMask
+#from AFQ.definitions.mapping import AffMap
+#from AFQ import api
 
 afd.organize_stanford_data()
 
-myafq = api.AFQ(bids_path=op.join(afd.afq_home, 'stanford_hardi'),
-                dmriprep='vistasoft',
-                tracking_params={"n_seeds": 1,
-                                 "directions": "det",
-                                 "odf_model": "DTI",
-                                 "seed_mask": RoiMask()},
-                bundle_info=["ARC"],
-                mapping=AffMap(),
-                viz_backend='plotly_no_gif')
+# myafq = api.AFQ(bids_path=op.join(afd.afq_home, 'stanford_hardi'),
+#                 dmriprep='vistasoft',
+#                 tracking_params={"n_seeds": 1,
+#                                  "directions": "det",
+#                                  "odf_model": "DTI",
+#                                  "seed_mask": RoiMask()},
+#                 bundle_info=["ARC"],
+#                 mapping=AffMap(),
+#                 viz_backend='plotly_no_gif')
 
-myafq.export_all()
+# myafq.export_all()
